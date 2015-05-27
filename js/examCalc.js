c115d536823b5a352259d58e0c0a33a851d494d2
Youppi3 <youppi3@gmail.com>
2015-05-26 22:28:55 -0400

    organize and input sizing

---
+++ js/examCalc.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+ $("form").keyup(function(){
+  var inputs = $("input");
+  var numFill = 0;
+  for (var i=0; i<inputs.length; i++) {
+   if (inputs[i].value && inputs[i].disabled == false) {
+    numFill += 1;
+   }
+   if (inputs[i].disabled) {
+    inputs[i].value = "";
+   }
+  }
+  if (numFill == 4) {
+   for (var i=0; i<inputs.length; i++) {
+    if (!inputs[i].value) {
+     inputs[i].disabled = true;
+     switch (i) {
+      case 0:
+       inputs[0].value = (inputs[3].value - (inputs[4].value * inputs[2].value / 100) - (((100 - inputs[2].value) / 2) * inputs[1].value / 100)) / ((100 - inputs[2].value) / 2) * 100;
+       break;
+      case 1:
+       inputs[1].value = (inputs[3].value - (inputs[4].value * inputs[2].value / 100) - (((100 - inputs[2].value) / 2) * inputs[0].value / 100)) / ((100 - inputs[2].value) / 2) * 100;
+       break;
+      case 2:
+       inputs[2].disabled = false;
+       break;
+      case 3:
+       inputs[3].value = (((100 - inputs[2].value) / 2) * inputs[0].value / 100) + (((100 - inputs[2].value) / 2) * inputs[1].value / 100) + (inputs[4].value * inputs[2].value / 100);
+       break;
+      case 4:
+       var tempvalue = ((inputs[3].value * 100) - (((100 - inputs[2].value) / 2) * inputs[0].value) - (((100 - inputs[2].value) / 2) * inputs[1].value)) / inputs[2].value;
+       inputs[4].value = tempvalue > 0 ? tempvalue : 0
+       break;
+      default:
+       break;
+     }
+     if (inputs[i].value > 100) {
+      $(inputs[i]).parent().addClass("has-error");
+      $(inputs[i]).parent().removeClass("has-warning");
+     } else if (i == 4 && inputs[i].value > Math.max(inputs[0].value,inputs[1].value)) {
+      $(inputs[i]).parent().addClass("has-warning");
+      $(inputs[i]).parent().removeClass("has-error");
+     } else {
+      $(inputs[i]).parent().removeClass("has-error");
+      $(inputs[i]).parent().removeClass("has-warning");
+     }
+    }
+   }
+  } else {
+   for (var i=0; i<inputs.length; i++) {
+    if (inputs[i].disabled) {
+     inputs[i].value = "";
+     inputs[i].disabled = false;
+     $(inputs[i]).parent().removeClass("has-error");
+     $(inputs[i]).parent().removeClass("has-warning");
+    }
+   }
+  }
+ });
+});
